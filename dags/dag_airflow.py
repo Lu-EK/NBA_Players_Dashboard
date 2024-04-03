ae62e966b400fb91bb7d4a82ed8f5230e542e573
Lucas Ellong Kotto <lucasek121@gmail.com>
2024-04-03 10:16:28 +0200

    final version v2

---
+++ dags/dag_airflow.py
@@ -0,0 +1,16 @@
+import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="daily_actualization",
+    schedule="0 8 * * *",
+    catchup=False,
+    start_date=datetime.datetime(2024, 4, 3),
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    ) as dag:
+    actualize = BashOperator(
+        task_id="actualize",
+        bash_command="python3 '/home/lucas/Data Science/Project NBA/actualize.py'"
+        )
+
