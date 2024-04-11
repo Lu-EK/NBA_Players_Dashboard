1fb5192a94ba5fcd3f6a37fe19cf2996c7e0e805
Lucas Ellong Kotto <lucasek121@gmail.com>
2024-04-11 19:59:22 +0200

    Project v1

--- init.py
+++ init.py
@@ -1,21 +1,21 @@
+import cProfile
 import datetime as dt
-import os
-import time
-import sys
-import subprocess
-import types
 import io
+import os
 import pstats
+import subprocess
+import sys
+import tempfile
+import time
+import types
 from typing import List
 from urllib.parse import quote_plus
 
 import altair as alt
 import duckdb
 import pandas as pd
-import tempfile
 import plotly.express as px
 import requests
-import cProfile
 import streamlit as st
 import streamlit.components.v1 as components
 from bs4 import BeautifulSoup
@@ -28,42 +28,44 @@ from streamlit_modal import Modal
 from streamlit_searchbox import st_searchbox
 from streamlit_theme import st_theme
 
-from etl import download_csv_from_bucket, upload_to_bucket, check_file_exists
+from etl import check_file_exists, download_csv_from_bucket, upload_to_bucket
 
 START_YEAR = 2005
 END_YEAR = 2023
-DB_NAME = 'stats.duckdb'
+DB_NAME = "stats.duckdb"
+
 
 def init_db():
-        conn = duckdb.connect(database=DB_NAME, read_only=False)
-        for year in range(START_YEAR, END_YEAR + 1):
-            players_stats_csv = download_csv_from_bucket(
-                "nba_dashboard_files", f"regular_dataset_{year - 1}_{year}.csv"
-            )
-            players_stats_ranked_csv = download_csv_from_bucket(
-                "nba_dashboard_files", f"ranked_dataset_{year - 1}_{year}.csv"
-            )
+    conn = duckdb.connect(database=DB_NAME, read_only=False)
+    for year in range(START_YEAR, END_YEAR + 1):
+        players_stats_csv = download_csv_from_bucket(
+            "nba_dashboard_files", f"regular_dataset_{year - 1}_{year}.csv"
+        )
+        players_stats_ranked_csv = download_csv_from_bucket(
+            "nba_dashboard_files", f"ranked_dataset_{year - 1}_{year}.csv"
+        )
 
-            players_stats = pd.read_csv(io.BytesIO(players_stats_csv))
-            players_stats_ranked = pd.read_csv(io.BytesIO(players_stats_ranked_csv))
-            # Create tables in DuckDB
-            # print(players_stats)
-            conn.register("players_stats_" + str(year) + "_" + str(year + 1), players_stats)
-            conn.register(
-                "players_stats_ranked_" + str(year) + "_" + str(year + 1),
-                players_stats_ranked,
-            )
+        players_stats = pd.read_csv(io.BytesIO(players_stats_csv))
+        players_stats_ranked = pd.read_csv(io.BytesIO(players_stats_ranked_csv))
+        # Create tables in DuckDB
+        # print(players_stats)
+        conn.register("players_stats_" + str(year) + "_" + str(year + 1), players_stats)
+        conn.register(
+            "players_stats_ranked_" + str(year) + "_" + str(year + 1),
+            players_stats_ranked,
+        )
 
-            # Get the list of all players
-            all_players_query = conn.execute(
-                f"SELECT player FROM players_stats_{year}_{year + 1} ORDER BY player"
-            ).fetchall()
-            all_players = [row[0] for row in all_players_query]
+        # Get the list of all players
+        all_players_query = conn.execute(
+            f"SELECT player FROM players_stats_{year}_{year + 1} ORDER BY player"
+        ).fetchall()
+        all_players = [row[0] for row in all_players_query]
 
-            all_players_df = pd.DataFrame({"player": all_players})
-            conn.register("all_players_" + str(year) + "_" + str(year + 1), all_players_df)
+        all_players_df = pd.DataFrame({"player": all_players})
+        conn.register("all_players_" + str(year) + "_" + str(year + 1), all_players_df)
 
-        return conn
+    return conn
+
 
 if __name__ == "__main__":
     time.sleep(1)
